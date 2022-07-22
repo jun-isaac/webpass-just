ffc4e8fb80eb774eef6a1f9166210b1a4aba9a31
jun-isaac <100373800+jun-isaac@users.noreply.github.com>
2022-07-22 10:41:58 +0900

    배너 추가, click 이벤트 추가

--- script/main.js
+++ script/main.js
@@ -39,4 +39,19 @@ $(function () {
 		changeSlider(num);
 	}, 3000)
 
+	// 공지사항/갤러리 tab 버튼
+	$('.tab_buttons .notice-btn').click(function(){
+		// 탭 초기화
+		$('.tab_contents').removeClass('on');
+		// 표시
+		$('#notice').addClass('on');
+	});
+
+	$('.tab_buttons .gallery-btn').click(function(){
+				// 탭 초기화
+				$('.tab_contents').removeClass('on');
+				// 표시
+		$('#gallery').addClass('on');
+	});
+
 }); //$
